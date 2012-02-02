8868523f6fde979c76186848b5eafaf5e768454a
IndexKey <xinsuhui@gmail.com>
2012-02-02 15:38:36 +0800

    移除String简单返回响应的.将资源的返回封装到HttpResult 类中

--- CnblogsIng/src/com/cnblogs/keyindex/service/ResourceExplorer.java
+++ CnblogsIng/src/com/cnblogs/keyindex/service/ResourceExplorer.java
@@ -5,61 +5,69 @@ import java.util.List;
 import org.apache.http.client.CookieStore;
 import org.apache.http.message.BasicNameValuePair;
 
-import com.cnblogs.keyindex.model.res.Resource;
 import com.cnblogs.keyindex.net.HttpHelper;
+import com.cnblogs.keyindex.net.HttpResult;
+import com.cnblogs.keyindex.response.res.Resource;
 import com.cnblogs.keyindex.serializers.Serializer;
 import com.cnblogs.keyindex.serializers.SerializerFactory;
 
 public class ResourceExplorer {
-	private String responseContent = null;
-	private Resource responseResource=null;
+	private Resource serializResult = null;
+	private HttpResult response = null;
 
-	public String getResponse() {
-		return responseContent;
-	}
-	
-	public Resource getResponseResource()
-	{
-		return responseResource;
+	public HttpResult getResponse() {
+		return response;
 	}
 
-	private String download(String uri, List<BasicNameValuePair> forms,
+	public Resource getResponseResource() {
+		return serializResult;
+	}
+
+	private HttpResult bulidResult(String html, HttpHelper helper) {
+		HttpResult result = new HttpResult();
+		result.setContentHtml(html);
+		result.setCookieStore(helper.getCookieStore());
+		result.setResponceHead(helper.getResponseHead());
+		return result;
+	}
+
+	private HttpResult download(String uri, List<BasicNameValuePair> forms,
 			CookieStore cookieStore) {
-
-		String result=null;
+		HttpResult result = new HttpResult();
+		String html = null;
 		HttpHelper httpHelper = new HttpHelper();
 		httpHelper.setCookieStore(cookieStore);
 		if (forms == null) {
+			html = httpHelper.getMethod(uri);
 
-			result = httpHelper.getMethod(uri);
 		} else {
-			result = httpHelper.postMethod(uri, forms);
+
+			html = httpHelper.postMethod(uri, forms);
 		}
+		result = bulidResult(html, httpHelper);
 		return result;
 
 	}
 
 	public ResourceExplorer getResource(String uri) {
-		responseContent = download(uri, null, null);
+		response = download(uri, null, null);
 		return this;
 	}
 
 	public ResourceExplorer getResource(String uri, CookieStore cookieStore) {
-		responseContent = download(uri, null, cookieStore);
+		response = download(uri, null, cookieStore);
 		return this;
 	}
 
 	public ResourceExplorer getResource(String uri,
 			List<BasicNameValuePair> forms, CookieStore cookieStore) {
-		responseContent = download(uri, forms, cookieStore);
+		response = download(uri, forms, cookieStore);
 		return this;
 	}
-	
-	
-	public ResourceExplorer serializerResult(String className)
-	{
-		Serializer format=SerializerFactory.CreateSerializer(className);
-		responseResource=format.format(responseContent);
+
+	public ResourceExplorer serializerResult(String className) {
+		Serializer format = SerializerFactory.CreateSerializer(className);
+		serializResult = format.format(response);
 		return this;
 	}
 
