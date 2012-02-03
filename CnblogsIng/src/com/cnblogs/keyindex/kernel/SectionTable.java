4e0567b00108905c67392deff06da6e368ae7ad0
IndexKey <xinsuhui@gmail.com>
2012-02-03 09:47:04 +0800

    修改Section 的存储数据结构,由于注册Section 时有序,将Map 更换为List. 下次修改建议 实现Map接口

--- CnblogsIng/src/com/cnblogs/keyindex/kernel/SectionTable.java
+++ CnblogsIng/src/com/cnblogs/keyindex/kernel/SectionTable.java
@@ -1,40 +1,45 @@
 package com.cnblogs.keyindex.kernel;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import com.cnblogs.keyindex.model.Section;
 
 
  class SectionTable {
 
-	private HashMap<String, Section> map;
+	 private List<Section> sections;
+	 private List<String> keys;
 
 	public SectionTable() {
-		map = new HashMap<String, Section>();
+//		map = new HashMap<String, Section>();
+		sections=new ArrayList<Section>();
+		keys=new ArrayList<String>();
 	}
 
 	public void regSection(Section section) {
-		map.put(section.getAction(), section);
+		
+		sections.add(sections.size(), section);
+		keys.add(keys.size(),section.getAction());
 	}
 
 	public void clearSection() {
-		map.clear();
+		sections.clear();
+		keys.clear();
 	}
 	
 	public Section getSection(String key)
 	{
-		return map.get(key);
+		int index=keys.indexOf(key);
+		if(index!=-1)
+			return sections.get(index);
+		return null;
 	}
 
 	public List<Section> getAllSection() {
-		List<Section> list = new ArrayList<Section>();
-		for (Map.Entry<String, Section> item : map.entrySet()) {
-			list.add(item.getValue());
-		}
-		return list;
+		
+		return sections;
 	}
 
 }
